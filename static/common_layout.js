c845acd920db93c6e11ac2317738682c23b5a207
patrickaxe <jean.facnet@gmail.com>
2026-08-26 17:05:31 -0300

    ofertas

--- static/common_layout.js
+++ static/common_layout.js
@@ -423,7 +423,7 @@ async function renderSharedTopbar() {
     <button type="button" onclick="window.location.href='/feed-page'">Feed</button>
 	<button type="button" onclick="window.location.href='/empregos'">Empregos</button>
 	<button type="button" onclick="window.location.href='/ofertas'">🔥 Ofertas</button>
-    <button type="button" onclick="window.location.href='${savedUser ? "/create-ad-page" : "/auth-page"}'">Anuncie-se</button>
+    <button type="button" onclick="window.location.href='${savedUser ? "/create-ad-page" : "/auth-page"}'">Anunciar</button>
     ${savedUser ? `<button type="button" onclick="window.location.href='/profile-page'">Perfil</button>` : ""}
     <button type="button" class="vip-btn" onclick="window.location.href='/vip-page'">Tornar-se VIP</button>
     ${authButton}
@@ -435,7 +435,7 @@ async function renderSharedTopbar() {
     <button type="button" onclick="window.location.href='/feed-page'">Feed</button>
 	<button type="button" onclick="window.location.href='/empregos'">Empregos</button>
 	<button type="button" onclick="window.location.href='/ofertas'">🔥 Ofertas</button>
-    <button type="button" onclick="window.location.href='${savedUser ? "/create-ad-page" : "/auth-page"}'">Anuncie-se</button>
+    <button type="button" onclick="window.location.href='${savedUser ? "/create-ad-page" : "/auth-page"}'">Anunciar</button>
     ${savedUser ? `<button type="button" onclick="window.location.href='/profile-page'">Perfil</button>` : ""}
     <button type="button" class="vip-btn" onclick="window.location.href='/vip-page'">VIP</button>
     ${
